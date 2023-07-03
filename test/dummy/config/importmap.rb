c37ce3f3284587bf93a3659b35a5bcfb7bc3f5e1
Pedro D. Garcia Lopez <lorrydriveloper@gmail.com>
2023-07-03 08:13:03 +0100

    add import maps for JS

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.1/app/javascript/turbo/index.js'
+pin '@hotwired/turbo', to: 'https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js'
+pin '@rails/actioncable/src', to: 'https://ga.jspm.io/npm:@rails/actioncable@7.0.5/src/index.js'
