fc17820ed4ea7ca02ee335c97d722198b403413b
Pedro D. Garcia Lopez <lorrydriveloper@gmail.com>
2023-07-06 08:57:29 +0100

    create the turbo_stream media type and use it.

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -4,6 +4,10 @@ module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
 
+    initializer 'turbo_clone.media_type' do
+      Mime::Type.register 'text/vnd.turbo-stream.html', :turbo_stream
+    end
+
     initializer 'turbo_clone.helpers' do
       ActiveSupport.on_load(:action_controller_base) do
         helper TurboClone::FramesHelper
